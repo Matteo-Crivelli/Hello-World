4013cbfe915a704e0f405c3ef89bb9bca9420ed6
Matteo-Crivelli <crivellimatteo04@gmail.com>
2025-08-28 11:07:44 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+
+{
+ std::cout << "Hello World\n";
+ std::cout << "i'm...\n";
+ std::cout << "Future genius, billionaire, philanthropist\n";
+ std::cout << "(Cit. Tony Stark)\n";
+ return 0;
+  }
